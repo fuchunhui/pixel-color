e80b4fda53135e54f50d86229ebc4219a553e5ab
fuchunhui <fuchuhu7@gmail.com>
2021-11-08 19:35:57 +0800

    refactor: remove useless code

--- index.d.ts
+++
@@ -1,3 +0,0 @@
-declare module 'pixel-color' { // TODO
-  export function load(container: string | Element, domain: string): any;
-}
--- src/types/color.ts
+++ src/types/color.ts
@@ -1,10 +1,3 @@
-export interface PropertyValue {
-  max: number;
-  size: number;
-  color: string;
-  align: string;
-}
-
 export interface BaseFile {
   name: string;
   base64: string
